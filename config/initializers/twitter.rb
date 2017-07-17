2f5341d5a073e8da26a9790bf319ed63081d4197
Shiva Bhusal <hotline.shiva@gmail.com>
2017-07-17 23:44:51 +0545

    Passes the failing test to create Tweet Object from Twitter::Tweet

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+TwitterClient = Twitter::REST::Client.new do |config|
+  config.consumer_key         = ENV['TWITTER_API']
+  config.consumer_secret      = ENV['TWITTER_API_SECRET']
+  config.access_token_secret  = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token         = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+end
