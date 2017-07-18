d8c8574b25f333456cf6100ad06986b01201d72f
Shiva Bhusal <hotline.shiva@gmail.com>
2017-07-18 10:55:40 +0545

    Creates new dashboard controller and separate layout initialized not fully setup though

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('vendor/startbootstrap-
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( static.js static.css )
+Rails.application.config.assets.precompile += %w[dashboard.js
+                                                 dashboard.css
+                                                 static.js
+                                                 static.css]
