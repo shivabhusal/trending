0c57e704171402c10379d6fda2f9861d12ee4df3
Shiva Bhusal <hotline.shiva@gmail.com>
2017-07-18 10:46:35 +0545

    Fixes Signup via twitter issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,23 +20,23 @@
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
-
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:twitter]
-         
-   def self.from_omniauth(auth)
-     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-       user.email = auth.info.email
-       user.password = Devise.friendly_token[0,20]
-       user.name = auth.info.name   # assuming the user model has a name
-       user.image = auth.info.image # assuming the user model has an image
-       # If you are using confirmable and the provider(s) you use validate emails, 
-       # uncomment the line below to skip the confirmation emails.
-       # user.skip_confirmation!
-     end
-   end
+         :omniauthable, omniauth_providers: [:twitter]
+
+  def self.from_omniauth_twitter(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      # Since twitter does not provide us email address in response
+      user.email       = "#{auth.info.nickname}@trending.io"
+      user.password    = Devise.friendly_token[0, 20]
+      user.first_name  = auth.info.first_name # assuming the user model has a name
+      user.last_name   = auth.info.last_name # assuming the user model has a name
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
 end
