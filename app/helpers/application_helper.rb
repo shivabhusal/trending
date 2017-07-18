995d15b52f1be938bc6b43710477513c51750679
Shiva Bhusal <hotline.shiva@gmail.com>
2017-07-18 17:06:58 +0545

    Some visual refinements in front-end and backend

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def embed_clickable_links_from(text)
+    simple_format(text)
+      .gsub(URI.regexp, '<a href="\0" target="_blank">\0</a>')
+      .html_safe
+  end
 end
