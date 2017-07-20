8471866444a4f95a1b0fac79b937c27dea4ff353
Shiva Bhusal <hotline.shiva@gmail.com>
2017-07-20 15:02:09 +0545

    Fixes after_signout_url for active admin

--- app/admin/login.rb
+++ app/admin/login.rb
@@ -1,6 +1,10 @@
-class ActiveAdmin::Devise::SessionsController
+ActiveAdmin::Devise::SessionsController.class_eval do
   def after_sign_in_path_for(resource)
     admin_dashboard_path
+  end
+
+  def root_path
+    "/"  #add your logic
   end
 
   def after_sign_out_path_for(resource_or_scope)
